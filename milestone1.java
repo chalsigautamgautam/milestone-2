81c89e8e134ba8f7aef1ae688eea0310c4a45fb5
chalsigautamgautam <110589898+chalsigautamgautam@users.noreply.github.com>
2022-08-04 22:10:45 +0530

    Add files via upload

---
+++ milestone1.java
@@ -0,0 +1,124 @@
+package testt;
+
+import java.util.*;
+
+class Employee implements Comparable<Employee>{
+	
+	int cid;
+	
+	String name;
+	
+	int salary;
+	
+	int age;
+	
+	Employee(int cid,String name,int salary,int age){
+		this.cid=cid;
+		this.name=name;
+		this.salary=salary;
+	    this.age=age;
+	}
+	public void setsalary(int cid ) {
+		this.salary = cid;
+	}
+	public int compareTo(Employee st) {
+		if(name==st.name)
+			return 0;
+		else if(name.compareTo(st.name)<0)
+			return 1;
+		else
+			return -1;
+	}
+}
+
+
+
+
+
+public class milestone1{
+	public static void main(String args[]) {
+		ArrayList<Employee> al=new ArrayList<Employee>();
+		
+		al.add(new Employee(101,"Chalsi",9000000,25 ));
+		al.add(new Employee(106,"Lucky",8000000,24 ));
+		al.add(new Employee(109,"Namrata",5000000,28 ));
+		al.add(new Employee(104,"Akbar",3000000,29 ));
+		al.add(new Employee(103,"Amar",4000000,30 ));
+		al.add(new Employee(105,"Abhishek",10000000,33 ));
+		al.add(new Employee(108,"Shreya",9500000,26 ));
+		al.add(new Employee(110,"Vishakha",4500000,43 ));
+		al.add(new Employee(102,"Varun",6700000,27 ));
+		al.add(new Employee(107,"Sid",8300000,19 ));
+		
+		
+		int len=al.size();
+		Scanner sc= new Scanner(System.in);
+		
+		
+		String whilechoice="go";
+		while (whilechoice=="go") {
+			
+			String avail="False";
+			System.out.print("Enter Your Choice");
+			System.out.print("1-->Employee Data. 2->EmployeeData in Descending Order. 3-->Update Employee Salary. 4-->Exit ");
+			System.out.print(" ");
+			int a= sc.nextInt();
+			
+			switch (a) {
+			
+			case 1:
+				System.out.println("");
+				System.out.println("Employee Data ..");
+				
+				System.out.println("Employee Name Salary Age");
+				
+				for(Employee st:al){
+					System.out.println(st.cid+" "+st.name+" "+st.salary+" "+st.age);
+				}
+				break;
+				
+				
+			case 2:
+				System.out.println("");
+				System.out.println("Employee Data in Descending Order.. ");
+				
+				System.out.println("Employee Data in Descending Order.. ");
+				Collections.sort(al);
+				for(Employee st:al) {
+					System.out.println(st.cid+" "+st.name+" "+st.salary+" "+st.age);
+				}
+					break;
+					case 3:
+						System.out.println("");
+						System.out.println("Enter Employee id to update salary.. ");
+						
+						int b= sc.nextInt();
+						System.out.println("Enter the new Salary.. ");
+
+						int c= sc.nextInt();
+						
+						
+						for(int i=0; i<len; i++) {
+							if (al.get(i).cid==b) {
+								al.get(i).setsalary(c);
+								avail="True";
+								System.out.println("Updated Salary.." + al.get(i).cid+" "+al.get(i).name+" "+al.get(i).salary+" "+al.get(i).age);
+							}
+							if("False".equals(avail)) {
+								System.out.print("Invalid Employee ID");
+							}
+						}
+							
+							break;
+							
+							
+							case 4:
+							System.out.println("");
+							whilechoice="stop";
+							System.out.println("Exiting ");
+							break;
+							}
+		}
+	}
+}
+	
